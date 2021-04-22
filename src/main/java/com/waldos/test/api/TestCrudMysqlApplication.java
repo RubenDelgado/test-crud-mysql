bb905995b39fe0d94c721694d8e08fb38c59fa28
RubenDelgado <rdelgado.stc@gmail.com>
2021-04-21 19:06:26 -0500

    Primer commit

---
+++ src/main/java/com/waldos/test/api/TestCrudMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.waldos.test.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCrudMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCrudMysqlApplication.class, args);
+	}
+
+}
